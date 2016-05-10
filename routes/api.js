361dafb6cc1ca7d0c6242208e5ca33ccfd5d4e05
Steve Ellis <email@steveell.is>
2016-05-10 18:42:39 -0400

    Use optimizer

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/compile', function(req, res, next) {
   var solidity = req.body.solidity;
-  var compiled = solc.compile(solidity);
+  var compiled = solc.compile(solidity, 1);
 
   res.json(compiled)
 });
