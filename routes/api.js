fd81e852ef6890fd8a8249f3e0049f67210b55d3
Steve Ellis <email@steveell.is>
2017-01-16 17:05:59 -0500

    make it possible to pass multiple files

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/compile', function(req, res, next) {
-  var solidity = req.body.solidity;
-  var compiled = solc.compile(solidity, 1);
+  var compiled = solc.compile({sources: req.body}, 1);
 
   res.json(compiled)
 });
